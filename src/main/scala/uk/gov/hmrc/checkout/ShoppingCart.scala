a3396055173711bea05a1679d68cf773dd24c61f
Barnabas Szoke <barnabas@clearscore.com>
2017-03-17 15:57:14 +0000

    more refactoring, added tailrec annotation

--- src/main/scala/uk/gov/hmrc/checkout/ShoppingCart.scala
+++ src/main/scala/uk/gov/hmrc/checkout/ShoppingCart.scala
@@ -1,5 +1,6 @@
 package uk.gov.hmrc.checkout
 
+import scala.annotation.tailrec
 import scala.math.BigDecimal.RoundingMode
 
 class ShoppingCart {
@@ -14,23 +15,27 @@ class ShoppingCart {
     Offer("Orange", 3, BigDecimal(2.0 / 3.0))
   )
 
-  private def costOfProdGroup(group: (String, Int)): BigDecimal = {
+  private def sumOfProductGroup(group: (String, Int)): BigDecimal = {
 
+    @tailrec
     def sum(group: (String, Int), cost: BigDecimal): BigDecimal = {
       val productName = group._1
       val count = group._2
 
-      if (count == 0) return cost
-
-      val foundOffer = offers.find(offer => offer.productName == productName && offer.quantity <= count)
-      val (processedElements: Int, costOfItems: BigDecimal) = processElement(productName, foundOffer)
-      sum((productName, count - processedElements), cost + costOfItems)
+      count match {
+        case 0 => cost
+        case _ => {
+          val foundOffer = offers.find(offer => offer.productName == productName && offer.quantity <= count)
+          val (processedElements: Int, costOfItems: BigDecimal) = calculatePrice(productName, foundOffer)
+          sum((productName, count - processedElements), cost + costOfItems)
+        }
+      }
     }
 
     sum(group, 0.0)
   }
 
-  private def processElement(productName: String, foundOffer: Option[Offer]): (Int, BigDecimal) = {
+  private def calculatePrice(productName: String, foundOffer: Option[Offer]): (Int, BigDecimal) = {
     foundOffer.map(offer => {
       val price = offer.quantity * productCatalog.getOrElse(productName, BigDecimal(0.0)) * offer.discount
       (offer.quantity, price)
@@ -42,7 +47,7 @@ class ShoppingCart {
   def totalCost(items: Seq[String]): BigDecimal = {
     val sum = items
       .groupBy(identity)
-      .map { case (a, b) => costOfProdGroup(a, b.size) }
+      .map { case (a, b) => sumOfProductGroup(a, b.size) }
       .sum
 
     sum.setScale(2, RoundingMode.CEILING)
